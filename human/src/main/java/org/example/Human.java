0bbdc18bf406614e3cf462a5dbfb3fe433e81841
Heimdall7358 <evil_heimdall58@mail.ru>
2024-03-02 22:45:37 +0300

    Правки класса Human

--- human/src/main/java/org/example/Human.java
+++ human/src/main/java/org/example/Human.java
@@ -2,39 +2,42 @@ package org.example;
 
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-import org.apache.commons.lang3.StringUtils;
 import org.example.exceptions.EqualsGenderParentsException;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.commons.lang3.StringUtils.capitalize;
+
 @Getter
 @EqualsAndHashCode(exclude = {"father", "mother", "children"})
 @ToString(of = {"name", "surname", "patronymic", "gender"})
 @FieldDefaults(level=AccessLevel.PRIVATE)
 public class Human {
-    final String name;
-    final String surname;
-    final String patronymic;
-    final Gender gender;
+    String name;
+    String surname;
+    String patronymic;
+    Gender gender;
     Human father;
     Human mother;
-    final Set<Human> children = new HashSet<>();
+    List<Human> children = new ArrayList<>();
 
     public Human(@NonNull String name, @NonNull String surname, @NonNull String patronymic, @NonNull Gender gender) {
-        this.name = StringUtils.capitalize(name);
-        this.surname = StringUtils.capitalize(surname);
-        this.patronymic = StringUtils.capitalize(patronymic);
+        this.name = capitalize(name);
+        this.surname = capitalize(surname);
+        this.patronymic = capitalize(patronymic);
         this.gender = gender;
     }
 
-    private void addParents(Human parent1,Human parent2) {
-        if (parent1.getGender().equals(Gender.MAN)) {
-            father = parent1;
-            mother = parent2;
-        } else {
-            father = parent2;
-            mother = parent1;
+    private void addParents(Human father,Human mother) {
+        if (father.getGender().equals(mother.getGender())) {
+            throw new IllegalArgumentException("Родители должны быть разного пола");
         }
+        this.father = father;
+        this.mother = mother;
+
+        father.getChildren().add(this);
+        mother.getChildren().add(this);
     }
 
     private void addChild(Human child) {
@@ -42,7 +45,7 @@ public class Human {
     }
 
     public Human createChild(@NonNull String name, @NonNull String surname, @NonNull String patronymic,
-                             @NonNull Gender gender,@NonNull Human parent){
+                             @NonNull Gender gender,@NonNull Human parent) throws EqualsGenderParentsException {
         if (!this.getGender().equals(parent.getGender())) {
             Human child = new Human(name, surname, patronymic, gender);
             child.addParents(this, parent);
@@ -54,4 +57,7 @@ public class Human {
         }
     }
 
+    public String getFullName() {
+        return String.format("%s %s %s", surname, name, patronymic);
+    }
 }
