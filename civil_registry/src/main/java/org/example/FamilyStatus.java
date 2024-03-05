4e07f10acd6c1472f9c8fab7e7d4258cbbeb4100
Roman Dobrokhotov <dobro-ra@yandex.ru>
2024-03-05 17:52:26 +0300

    ЗАГС - v4.1

--- civil_registry/src/main/java/org/example/FamilyStatus.java
+++ civil_registry/src/main/java/org/example/FamilyStatus.java
@@ -5,9 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-@NoArgsConstructor
-@Getter
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public enum FamilyStatus {
     NOT_MARRIED, MARRIED, DIVORSED
 }
